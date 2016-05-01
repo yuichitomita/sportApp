e745464c9e7a93c0bc7f97add5568b175a15fda6
ki0802kgik <ki0802kgik@gmail.com>
2016-05-01 18:54:32 +0900

    web view 追加

--- sportApp/ListeningViewController.swift
+++ sportApp/ListeningViewController.swift
@@ -8,14 +8,29 @@
 
 import UIKit
 
-class ListeningViewController: UIViewController {
+class ListeningViewController: UIViewController ,UIWebViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //画面一杯にWebを表示
+        let myWebView : UIWebView = UIWebView()
+        myWebView.delegate = self
+        myWebView.frame = self.view.bounds
+        self.view.addSubview(myWebView)
+        let url: NSURL = NSURL(string: "http://153.126.157.154:83/index2.html")!
+        let request: NSURLRequest = NSURLRequest(URL: url)
+        myWebView.loadRequest(request)
     }
-
+    //ページが読み終わったときに呼ばれる関数
+    func webViewDidFinishLoad(webView: UIWebView) {
+        print("ページ読み込み完了しました！")
+    }
+    //ページを読み始めた時に呼ばれる関数
+    func webViewDidStartLoad(webView: UIWebView) {
+        print("ページ読み込み開始しました！")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
