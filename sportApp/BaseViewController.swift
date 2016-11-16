91aae668accad757ec41bfeea63eff648989fd78
yuichitomita <y.tomita@speedlink-td.info>
2016-11-16 23:05:58 +0900

    不要なコメント削除

--- sportApp/BaseViewController.swift
+++ sportApp/BaseViewController.swift
@@ -12,12 +12,12 @@ class BaseViewController: UIViewController, SlideMenuDeligate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
     func slideMenuItemSelectedAtIndex(_ index: Int32) {
