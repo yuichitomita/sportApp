9e01f3bb5f942d9d64057f8e6b0e7f20f45f2578
yuichitomita <y.tomita@speedlink-td.info>
2016-11-23 01:50:17 +0900

    動的にセルを生成するように変更

--- sportApp/ListLViewController.swift
+++ sportApp/ListLViewController.swift
@@ -12,6 +12,12 @@ class ListLViewController: UIViewController ,UITableViewDelegate, UITableViewDat
     
  
     @IBOutlet var tableView: UITableView!
+    
+    let titles = ["放送Title","のんびりタイムハート","寂しがり屋のラジオ","若林がお届けします","アゴトーク"]
+    let accountNames = ["アカウント名","みさきち","寂しがり屋","若林","あごだし"]
+    let accountImages = ["account1","account2","account3","account4","account5"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,21 +31,23 @@ class ListLViewController: UIViewController ,UITableViewDelegate, UITableViewDat
         super.didReceiveMemoryWarning()
     }
     
-    //TableViewのセクション数を返すメソッド
     private func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return titles.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // Storyboardで設定したIdentifierでUITableViewCellのインスタンスを生成
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath as IndexPath)
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath as IndexPath) as! CustomTableViewCell
+        
+        cell.title.text = titles[indexPath.row]
+        cell.accountName.text = accountNames[indexPath.row]
+        cell.accountImageView.image = UIImage(named: accountImages[indexPath.row])
         
         return cell
     }
-
 
 }
