f49e52b7562a8b19b061ea3793ba942099869187
yuichitomita <y.tomita@speedlink-td.info>
2016-12-13 21:25:13 +0900

    loginViewController追加

---
+++ sportApp/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  sportApp
+//
+//  Created by Tomi on 2016/12/13.
+//  Copyright © 2016年 slj. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+       
+    }
+    
+
+}
