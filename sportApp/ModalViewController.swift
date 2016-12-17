11f82e78c6f991b93ddd09342b6fb6ea64c5dbf6
yuichitomita <y.tomita@speedlink-td.info>
2016-12-17 22:07:22 +0900

    モーダル用ViewController追加

---
+++ sportApp/ModalViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ModalViewController.swift
+//  sportApp
+//
+//  Created by Tomi on 2016/12/11.
+//  Copyright © 2016年 slj. All rights reserved.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    
+    }
+
+
+}
